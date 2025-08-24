196e8a1ae818e97bddf07858961b664be434b308
Matt Hadden <forceh2010@gmail.com>
2025-08-24 15:39:50 -0400

    chore: remove mismatched global

--- CMR2Decomp/TimingUtils.h
+++ CMR2Decomp/TimingUtils.h
@@ -2,7 +2,6 @@
 
 #include <cstdint>
 
-// GLOBAL: CMR2 0x00519f58
 const char* g_minSecMSECFormatString = "%02d:%02d.%02d";
 
 void __stdcall FormatCentisecondsAsMinSecMSec(int32_t iTime, char* pcFormattedTime);
