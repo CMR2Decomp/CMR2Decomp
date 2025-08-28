a2f0c494211a6d1d318ad1b26a2f05f952cb9485
Matt Hadden <forceh2010@gmail.com>
2025-08-28 14:49:56 -0400

    Move GameInfo funcs to a class

--- CMR2Decomp/GameInfo.cpp
+++ CMR2Decomp/GameInfo.cpp
@@ -1,30 +1,52 @@
 #include "GameInfo.h"
 
-// GLOBAL: CMR2 0x0052afa0
-GameInfo g_gameInfo;
-
-// GLOBAL: CMR2 0x0052ea54
-unsigned int g_gameRegion;
+GameInfo CGameInfo::m_gameInfo;
+unsigned int CGameInfo::m_gameRegion;
+char CGameInfo::m_installedFrontendDir[260];
 
 // FUNCTION: CMR2 0x004057f0
-unsigned char GetGameLanguage(void)
+unsigned char CGameInfo::GetGameLanguage(void)
 {
-    return g_gameInfo.field2_0x14 & 7;
+    return m_gameInfo.field2_0x14 & 7;
 }
 
 // FUNCTION: CMR2 0x00405800
-unsigned int GetGameRegion(void)
+unsigned int CGameInfo::GetGameRegion(void)
 {
-    return g_gameRegion;
+    return m_gameRegion;
 }
 
 // FUNCTION: CMR2 0x00405d80
-unsigned char FUN_00405d80(void)
+unsigned char CGameInfo::FUN_00405d80(void)
 {
-    return g_gameInfo.field2_0x14 >> 3 & 0x7f;
+    return m_gameInfo.field2_0x14 >> 3 & 0x7f;
 }
 
-unsigned char FUN_00405d90(void)
+unsigned char CGameInfo::FUN_00405d90(void)
 {
-    return g_gameInfo.field2_0x14 >> 10 & 7;
+    return m_gameInfo.field2_0x14 >> 10 & 7;
+}
+
+// FUNCTION: CMR2 0x00405c10
+int __stdcall CGameInfo::GetScreenWidth(void)
+{
+    return m_gameInfo.screenWidth;
+}
+
+// FUNCTION: CMR2 0x00405c30
+int __stdcall CGameInfo::GetScreenHeight(void)
+{
+    return m_gameInfo.screenHeight;
+}
+
+// FUNCTION: CMR2 0x00405c50
+int __stdcall CGameInfo::GetColourDepth(void)
+{
+    return m_gameInfo.screenColourDepth;
+}
+
+// FUNCTION: CMR2 0x0040ed90
+char *__stdcall CGameInfo::GetInstalledFrontendDir()
+{
+    return m_installedFrontendDir;
 }
