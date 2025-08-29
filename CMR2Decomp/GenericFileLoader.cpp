11661b751e115b63280f70bf1003a513e6fa6c7d
Matt Hadden <forceh2010@gmail.com>
2025-08-29 16:52:17 -0400

    Improve CGenericFileLoader

--- CMR2Decomp/GenericFileLoader.cpp
+++ CMR2Decomp/GenericFileLoader.cpp
@@ -1,18 +1,42 @@
 #include "GenericFileLoader.h"
 
 GenericFile CGenericFileLoader::m_genericFile;
+int CGenericFileLoader::m_unk0x00663fe8;
 
-// STUB: CMR2 0x004a9d70
-unsigned int __stdcall CGenericFileLoader::FUN_004a9d70(GenericFile *file, char *fileName)
+// FUNCTION: CMR2 0x004a9d70
+bool __stdcall CGenericFileLoader::FUN_004a9d70(GenericFile *file, char *buffer)
 {
-    unsigned int uVar1 = 0;
+    file->didFileLoad = FALSE;
+    if (FUN_004a9c30(buffer, file) != FALSE)
+        return file->didFileLoad = TRUE;
 
-    file->didFileLoad = 0;
-    // uVar1 = FUN_004a9c30(param_2, &file->buffer);
-    if (uVar1 != '\0')
+    return false;
+}
+
+// FUNCTION: CMR2 0x004a9c30
+bool __stdcall CGenericFileLoader::FUN_004a9c30(void *param_1, GenericFile *param_2)
+{
+    param_2->buffer = GetGenericFileBuffer(param_1, 0);
+    if (param_2->buffer != NULL)
     {
-        file->didFileLoad = 1;
-        return 1;
+        param_2->fileSize = GetGenericFileSize();
+        return true;
     }
-    return uVar1 & 0xffffff00;
+
+    param_2->fileSize = 0;
+    return false;
+}
+
+// STUB: CMR2 0x004aa220
+void *__stdcall CGenericFileLoader::GetGenericFileBuffer(void *param1, BOOL param2)
+{
+    // todo: implementation
+    // todo: check param2
+    return 0;
+}
+
+// FUNCTION: CMR2 0x004aa590
+int CGenericFileLoader::GetGenericFileSize(void)
+{
+    return m_unk0x00663fe8;
 }
--- CMR2Decomp/GenericFileLoader.h
+++ CMR2Decomp/GenericFileLoader.h
@@ -15,9 +15,14 @@ class CGenericFileLoader
 public:
     // GLOBAL: CMR2 0x00818250
     static GenericFile m_genericFile;
+    // GLOBAL: CMR2 0x00663fe8
+    static int m_unk0x00663fe8;
 
     // TODO: should this not be a static class? seems silly to pass this in like this
-    static unsigned int __stdcall FUN_004a9d70(GenericFile *file, char *fileName);
+    static bool __stdcall FUN_004a9d70(GenericFile *file, char *fileName);
+    static bool __stdcall FUN_004a9c30(void *param_1, GenericFile *param_2);
+    static int GetGenericFileSize(void);
+    static void *__stdcall GetGenericFileBuffer(void *param1, BOOL param2);
 };
 
 #endif
