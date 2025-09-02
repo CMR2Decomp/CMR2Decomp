947ba9493b38953c1b3930d09079220e347ccb9c
Matt Hadden <forceh2010@gmail.com>
2025-09-02 18:48:39 -0400

    Add CGame::FUN_0049c2c0 and CGame::FUN_0049c150

--- CMR2Decomp/Game.cpp
+++ CMR2Decomp/Game.cpp
@@ -6,9 +6,13 @@ int CGame::m_unk0x00523c58 = -1;
 int CGame::m_unk0x00523c5c = -1;
 Unk0049c2c0 CGame::m_unk0x00817da0;
 int CGame::m_unk0x0052ea4c;
-bool CGame::m_unk0x00817eb0;
-BYTE CGame::m_unk0x00817d98;
+bool CGame::m_unk0x00817eb0 = false;
+Unk00817d98 CGame::m_unk0x00817d98;
 BYTE CGame::m_unk0x00523c18;
+
+FuncTableEntry CGame::m_funcTable[20] = {
+    &CGame::InitializeGame,
+};
 
 // FUNCTION: CMR2 0x004a9a40
 void CGame::SetShouldExit(void)
@@ -41,7 +45,7 @@ BOOL CGame::FUN_004d0780(void)
         }
 
         FUN_0049c150(&m_unk0x00817d98, 0, 0xFF);
-        FUN_0049c190(&m_unk0x00817da0, 1, &m_unk0x00817d98, InitializeGame, &m_unk0x00523c18);
+        FUN_0049c190(&m_unk0x00817da0, 1, &m_unk0x00817d98, m_funcTable, &m_unk0x00523c18);
         m_unk0x00817eb0 = true;
         return FALSE;
 
@@ -56,23 +60,54 @@ int CGame::FUN_004057d0(void)
     return m_unk0x0052ea4c;
 }
 
-// STUB: CMR2 0x00523bc8
-void CGame::InitializeGame(int p1, int p2) {}
+// STUB: CMR2 0x004d15e0
+void CGame::InitializeGame(Unk0049c2c0 *p1, BYTE p2)
+{
+    return;
+}
 
-// STUB: CMR2 0x0049c2c0
-void __stdcall CGame::FUN_0049c2c0(void *param1) {}
+// FUNCTION: CMR2 0x0049c2c0
+void __stdcall CGame::FUN_0049c2c0(Unk0049c2c0 *param1)
+{
+    FuncTableEntry func;
+    BYTE counter;
+    BYTE unkIx;
+
+    counter = 0;
+    unkIx = 0;
+    if (param1->count > 0)
+    {
+        do
+        {
+            func = param1->funcLookupTable[param1->unk[unkIx].field0x1 & 0xFF];
+            if (func != NULL)
+                (func)(param1, unkIx);
+
+            counter++;
+            unkIx = counter;
+        } while (counter < param1->count);
+    }
+}
+
 // STUB: CMR2 0x0049c310
 void __stdcall CGame::FUN_0049c310(void *param1) {}
 // STUB: CMR2 0x0049c370
 void __stdcall CGame::FUN_0049c370(void *param1) {}
-// STUB: CMR2 0x0049c150
-void __stdcall CGame::FUN_0049c150(void *param1, unsigned int param2, unsigned int param3) {}
+
+// FUNCTION: CMR2 0x0049c150
+void __stdcall CGame::FUN_0049c150(Unk00817d98 *param1, int param2, int param3)
+{
+    param1->field0x2 = 0;
+    param1->field0x1 =
+        (param1->field0x1 >> 8 & 0xfc0000) << 8 | param2 & 0xffU | (param3 & 0xffU) << 8;
+}
+
 // FUNCTION: CMR2 0x0049c190
-void __stdcall CGame::FUN_0049c190(Unk0049c2c0 *p1, BYTE count, BYTE *unk, void *funcPointer, void *unk2)
+void __stdcall CGame::FUN_0049c190(Unk0049c2c0 *p1, BYTE count, Unk00817d98 *unk, FuncTableEntry *funcLookupTable, void *unk2)
 {
     p1->count = count;
     p1->unk = unk;
-    p1->funcPointer = funcPointer;
+    p1->funcLookupTable = funcLookupTable;
     p1->unk2 = unk2;
 }
 // STUB: CMR2 0x004ff450
--- CMR2Decomp/Game.h
+++ CMR2Decomp/Game.h
@@ -3,13 +3,21 @@
 
 #include <windows.h>
 
+typedef void (*FuncTableEntry)(struct Unk0049c2c0 *, BYTE);
+
+struct Unk00817d98
+{
+    int field0x1;
+    int field0x2;
+};
+
 struct Unk0049c2c0
 {
     BYTE count;
-    BYTE pad[3];       // is this meant to be something?
-    BYTE *unk;         // pointer to an 8 byte struct
-    void *funcPointer; // initializegame for instance
-    void *unk2;        // pointer to something that is 64 bytes?
+    BYTE pad[3];                     // is this meant to be something?
+    Unk00817d98 *unk;                // pointer to an 8 byte struct?
+    FuncTableEntry *funcLookupTable; // pointer to the function table which points to functions like initializegame
+    void *unk2;                      // pointer to something that is 64 bytes?
 };
 
 class CGame
@@ -21,9 +29,9 @@ public:
     static void __stdcall FUN_0049c2c0(Unk0049c2c0 *param1);
     static void __stdcall FUN_0049c310(void *param1);
     static void __stdcall FUN_0049c370(void *param1);
-    static void __stdcall FUN_0049c150(void *param1, unsigned int param2, unsigned int param3);
-    static void __stdcall FUN_0049c190(Unk0049c2c0 *p1, BYTE count, BYTE *unk, void *funcPointer, void *unk2);
-    static void InitializeGame(int p1, int p2);
+    static void __stdcall FUN_0049c150(Unk00817d98 *param1, int param2, int param3);
+    static void __stdcall FUN_0049c190(Unk0049c2c0 *p1, BYTE count, Unk00817d98 *unk, FuncTableEntry *funcLookupTable, void *unk2);
+    static void InitializeGame(Unk0049c2c0 *p1, BYTE p2);
     static BOOL FUN_004ff450();
     static BOOL FUN_0041b060();
 
@@ -42,9 +50,11 @@ public:
     // GLOBAL: CMR2 0x00817da0
     static Unk0049c2c0 m_unk0x00817da0;
     // GLOBAL: CMR2 0x00817d98
-    static BYTE m_unk0x00817d98;
+    static Unk00817d98 m_unk0x00817d98;
     // GLOBAL: CMR2 0x00523c18
     static BYTE m_unk0x00523c18;
+    // GLOBAL: CMR2 0x00523bc8
+    static FuncTableEntry m_funcTable[20];
 };
 
 #endif
